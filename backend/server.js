6843a1e6cd453e3e0561184af2959de457452408
Ced <conquete.asie@gmail.com>
2021-04-17 21:45:09 +0200

    add colors dpend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import path from "path";
 import express from "express";
 import dotenv from "dotenv";
+import colors from "colors";
 import morgan from "morgan";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
@@ -52,9 +53,8 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 
 app.listen(
-  5000,
+  PORT,
   console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
-      .brightMagenta.bold
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
   )
 );
